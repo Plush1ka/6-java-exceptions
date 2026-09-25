46f8cd6c7ebc1bc0d68be65549a3a6e402b2ef3b
Plushika <plushika.core@gmail.com>
2026-09-25 16:54:52 +0500

    Валюженич Алексей. Задание 06

--- task06/src/main/java/com/example/task06/Task06Main.java
+++ task06/src/main/java/com/example/task06/Task06Main.java
@@ -4,13 +4,17 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         new Task06Main().printMethodName();
-         */
+
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        StackTraceElement[] traceElements = new Throwable().getStackTrace();
+
+        StackTraceElement caller = traceElements[1];
+
+        System.out.print(caller.getMethodName());
     }
 
 }
