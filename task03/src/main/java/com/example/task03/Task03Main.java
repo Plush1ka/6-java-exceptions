96cecb74aeedce91b321e5a99fbf968f29834845
Plushika <plushika.core@gmail.com>
2026-09-25 16:09:18 +0500

    Валюженич Алексей. Задание 03

--- task03/src/main/java/com/example/task03/Task03Main.java
+++ task03/src/main/java/com/example/task03/Task03Main.java
@@ -1,16 +1,20 @@
 package com.example.task03;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Task03Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         throwCheckedException();
-         */
+
     }
 
-    //todo напишите здесь свою корректную реализацию задания
-    public static void throwCheckedException() {
 
+    public static void throwCheckedException() throws FileNotFoundException{
+        throw new FileNotFoundException("Файл не найден.");
     }
 }
